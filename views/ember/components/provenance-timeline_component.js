5b1f0a8156a6a04b2e388ff16c69bd1f60c4e869
David Newbury <david.newbury@gmail.com>
2014-10-24 13:20:23 -0400

    Add lifespan bars to graph

--- views/ember/components/provenance-timeline_component.js
+++ views/ember/components/provenance-timeline_component.js
@@ -17,10 +17,6 @@ App.ProvenanceTimelineComponent = Ember.Component.extend({
     return this.get('height') - this.get('margin.top') - this.get('margin.bottom');
   }.property('height'),  
 
-  didInsertElement: function(){
-    this.draw();
-  },
-
   transformG: function(){
     return "translate(" + this.get('margin.left') + "," + this.get('margin.top') + ")";
   }.property(),
@@ -29,12 +25,15 @@ App.ProvenanceTimelineComponent = Ember.Component.extend({
     return "translate(0,"+ this.get('h') +")";
   }.property("h"),  
 
+  didInsertElement: function(){
+    this.draw();
+  },
+
   periodChanged: function() {
     this.draw();
   }.observes('timeline_data'),
 
   draw: function(){
-      
 
     var svg = d3.select('#'+this.get('elementId'));
     var data = this.get('timeline_data');
@@ -42,72 +41,93 @@ App.ProvenanceTimelineComponent = Ember.Component.extend({
     var width = this.get('w');
     var height = this.get('h');
    
+    // Set Y scales
     var y = d3.scale.ordinal().rangeRoundBands([height,0], 0.1);;
     y.domain(data.map(function(d) { return +d.get('order'); }));
 
+    // Set X scales
     var x = d3.time.scale().rangeRound([0,width],0.1);
     x.domain([this.get('creationDate'),Date.now()]);
     x.nice(d3.time.year);
 
-   var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(10);
-   svg.select(".axis.x").call(xAxis);
+    // Setup X Axis
+    var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(10);
+    svg.select(".axis.x").call(xAxis);
 
 
-    var periods = svg.select(".periods").selectAll("g").data(data)
+    // Build new elements 
+    var creates = svg.select(".periods").selectAll("g").data(data)
       .enter().append("g");
-    
-    periods.append("rect")
+     creates.append("rect") // Create possible bar
       .attr("class", "possible_bounds")
-      .attr("height", y.rangeBand()/2)
-      .attr("width", function(d) {
-        var val = (x(d.get("latest_possible")) - x(d.get("computed_earliest_possible")));
-        return val || 0;
-      })
-      .attr("y", function(d) { return y(d.get("order")) + y.rangeBand()/2; })  
-      .attr("x", function(d) { 
-        return x(d.get("computed_earliest_possible")); 
-      })  
-
-    periods.append("rect")
+    creates.append('polyline') // create lifespan bar
+      .attr("class", 'lifespan')
+    creates.append("rect") // Create definite bar
       .attr("class", "definite_bounds")
+    creates.append("rect")  // Create Start bar
+      .attr("class", "start_of_definite")
+      .attr("width", 1)
+    creates.append("rect")  // Create End bar
+      .attr("class", "end_of_definite")
+      .attr("width", 1)
+    creates.append('text') // Create label
+      .attr("dy", "1em") 
+  
+    // Update all elements
+    var updates = svg.select('.periods').data(data)
+   
+
+    updates.selectAll(".lifespan")
+      .attr("transform", function(d){
+        str  = "translate(" 
+        str += (x(d.get('birth'))+.5) + ","
+        str += (y(d.get("order"))+ y.rangeBand()/2 + .5) + ")"
+        return str;
+      })
+      .attr("points", function(d) {
+        if (!d.get('birth')) return ""
+        var p = ""
+        var len =  x(d.get('death')) - x(d.get('birth'));
+        return p + "0,5 0,-5 0,0 " + len + ",0 " + len +",5 " + len + ",-5";
+      })
+
+    // update definite range
+    updates.selectAll(".definite_bounds")
       .attr("height", y.rangeBand()/2)
       .attr("width", function(d) {
         var val =  x(d.get("latest_definite")) - x(d.get("earliest_definite"));
         return val || 0;
       })
       .attr("y", function(d) { return y(d.get("order")) + y.rangeBand()/2; })  
-      .attr("x", function(d) { 
-        return x(d.get("earliest_definite")) || -1000 ; 
-      })        
+      .attr("x", function(d) {  return x(d.get("earliest_definite")) || -1000 ;})   
 
-
-    periods.append("rect")  // Start bar
-      .attr("class", "start_of_definite")
-      .attr("height", y.rangeBand()/2 + 2)
-      .attr("width", 1)
-      .attr("y", function(d) { return y(d.get("order")) + y.rangeBand()/2 -1; })  
-      .attr("x", function(d) { 
-        return x(d.get("earliest_definite")) || -1000; 
+    // update possible range
+    updates.selectAll(".possible_bounds")
+      .attr("y", function(d) { return y(d.get("order")) + y.rangeBand()/2; })  
+      .attr("x", function(d) { return x(d.get("computed_earliest_possible")); })
+      .attr("height", y.rangeBand()/2)
+      .attr("width", function(d) {
+        var val = (x(d.get("latest_possible")) - x(d.get("computed_earliest_possible")));
+        return val || 0;
       })
 
-    periods.append("rect")  // End bar
-      .attr("class", "start_of_definite")
-      .attr("height", y.rangeBand()/2 + 2)
-      .attr("width", 1)
+    // update start bars
+    updates.selectAll(".start_of_definite")
       .attr("y", function(d) { return y(d.get("order")) + y.rangeBand()/2 -1; })  
-      .attr("x", function(d) { 
-        return x(d.get("latest_definite")) || -1000; 
-      })
+      .attr("x", function(d) { return x(d.get("earliest_definite")) || -1000; })
+      .attr("height", y.rangeBand()/2 + 2)
 
+    // update end bars
+    updates.selectAll(".end_of_definite")
+      .attr("height", y.rangeBand()/2 + 2)
+      .attr("y", function(d) { return y(d.get("order")) + y.rangeBand()/2 -1; })  
+      .attr("x", function(d) { return x(d.get("latest_definite")) || -1000;   })
 
-    periods.append('text')
-        .attr("class", function(d) {return d.get("active") ? "active" : "";})
-        .attr("y", function(d) {return y(d.get("order")) }) 
-        .attr("dy", "1em") 
-        .attr("x", function(d) { 
-          return x(d.get("computed_earliest_possible")); 
-        })  
-    svg.select('.periods').selectAll("text").data(data)
+    //Update text on change
+    updates.selectAll("text")
+      .attr("y", function(d) {return y(d.get("order")) }) 
+      .attr("x", function(d) { return x(d.get("computed_earliest_possible"));})  
+      .attr("class", function(d) {return d.get("active") ? "active" : "";})
       .text(function(d){return d.get("party")});
   },  
 });

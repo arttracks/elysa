f6a7b6b9047748552e2bcf8d0de768d44898b3b8
David Newbury <david.newbury@gmail.com>
2014-10-30 10:53:03 -0400

    Removed redundant action

--- views/ember/controllers/period_controller.js
+++ views/ember/controllers/period_controller.js
@@ -1,10 +1,6 @@
 App.PeriodController = Ember.ObjectController.extend({
   needs: ['artwork'],
-
   actions: {
-    refreshData: function() {
-      this.send('rebuildStructure');
-    },
     updateDate: function(val, field) {
       console.log("val",val, field);
     },

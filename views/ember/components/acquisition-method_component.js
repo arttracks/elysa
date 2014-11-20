795c909f3c5a30f966b26aae073640d47232d2e4
David Newbury <david.newbury@gmail.com>
2014-11-20 07:41:21 -0600

    Moved gift up in the lookup.  Closes #41.

--- views/ember/components/acquisition-method_component.js
+++ views/ember/components/acquisition-method_component.js
@@ -6,7 +6,7 @@ App.DynamicSelectView = Ember.Select.extend({
 });
 
 App.AcquisitionMethodComponent  = Ember.Component.extend({
-  acq_methods: ["Bequest", "By descent", "By descent through", "Sale", "Purchase", "Purchase via Agent", "Acquisition", "Auction", "Exchange", "Gift, by exchange", "Bequest, by exchange", "Gift", "Conversion", "Looting", "Forced Sale", "Restitution", "Transfer", "Commission", "Field Collection", "With", "For Sale", "In Sale", "As Agent"],
+  acq_methods: ["Bequest", "By descent", "By descent through", "Sale", "Purchase", "Purchase via Agent", "Acquisition", "Auction", "Exchange", "Gift", "Gift, by exchange", "Bequest, by exchange", "Conversion", "Looting", "Forced Sale", "Restitution", "Transfer", "Commission", "Field Collection", "With", "For Sale", "In Sale", "As Agent"],
   label: "Acq. Method",
   actions: {
     handleChange: function() {
